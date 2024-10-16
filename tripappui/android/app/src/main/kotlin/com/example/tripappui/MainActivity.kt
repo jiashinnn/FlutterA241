e1ae88f4c048e80ea10bc171ff7f8ebfbb238bf3
Jaslynnn64 <122892635+Jaslynnn64@users.noreply.github.com>
2024-10-16 16:30:23 +0800

    person generator

---
+++ tripappui/android/app/src/main/kotlin/com/example/tripappui/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tripappui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ tripappui/lib/main.dart
@@ -0,0 +1,204 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ));
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late PageController _pageController;
+
+  void _onScroll() {}
+
+  @override
+  void initState() {
+    _pageController = PageController(
+      initialPage: 0,
+    )..addListener(_onScroll);
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: PageView(
+      controller: _pageController,
+      children: <Widget>[
+        makePage(
+          image: 'assets/images/one.jpg',
+          title: 'SONGYUQI',
+          description: 'Always be proud of yourself',
+        ),
+        makePage(
+          image: 'assets/images/two.jpg',
+          title: '宋雨琦',
+          description: '先爱自己，再爱别人',
+        ),
+        makePage(
+          image: 'assets/images/three.jpg',
+          title: 'WUGI',
+          description: 'Standing tall like a giant',
+        ),
+        makePage(
+          image: 'assets/images/four.jpg',
+          title: '小饼干',
+          description: '会永远在你身后！',
+        ),
+      ],
+    ));
+  }
+
+  Widget makePage({image, title, description}) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(image),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.bottomRight,
+            stops: const [0.3, 0.9],
+            colors: [
+              Colors.black.withOpacity(0.9),
+              Colors.black.withOpacity(0.2),
+            ],
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const SizedBox(
+                height: 40,
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.baseline,
+                textBaseline: TextBaseline.alphabetic,
+                children: <Widget>[
+                  Text(
+                    '1',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    '/4',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 15,
+                    ),
+                  ),
+                ],
+              ),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      title,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 50,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Container(
+                          margin: const EdgeInsets.only(right: 3),
+                          child: const Icon(
+                            Icons.star,
+                            color: Colors.yellow,
+                            size: 15,
+                          ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.only(right: 3),
+                          child: const Icon(
+                            Icons.star,
+                            color: Colors.yellow,
+                            size: 15,
+                          ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.only(right: 3),
+                          child: const Icon(
+                            Icons.star,
+                            color: Colors.yellow,
+                            size: 15,
+                          ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.only(right: 3),
+                          child: const Icon(
+                            Icons.star,
+                            color: Colors.yellow,
+                            size: 15,
+                          ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.only(right: 5),
+                          child: const Icon(
+                            Icons.star,
+                            color: Colors.grey,
+                            size: 15,
+                          ),
+                        ),
+                        const Text(
+                          '4.0',
+                          style: TextStyle(color: Colors.white70),
+                        ),
+                        const Text(
+                          ' (2300)',
+                          style: TextStyle(color: Colors.white38, fontSize: 12),
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 50),
+                      child: Text(
+                        description,
+                        style: TextStyle(
+                          color: Colors.white.withOpacity(.7),
+                          height: 1.9,
+                          fontSize: 15),
+                        ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const Text(
+                      'READ MORE', style: TextStyle(color: Colors.white),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
