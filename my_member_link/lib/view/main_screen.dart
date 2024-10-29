f185699b31234918c8345178e08b8e302443442c
Jaslynnn64 <122892635+Jaslynnn64@users.noreply.github.com>
2024-10-29 16:39:11 +0800

    drawer

--- my_member_link/lib/view/main_screen.dart
+++ my_member_link/lib/view/main_screen.dart
@@ -12,7 +12,41 @@ class _MainScreenState extends State<MainScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: const Center(child: Text("MAIN SCREEN"),),
+      body: const Center(
+        child: Text("MAIN SCREEN"),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text("Drawer Header"),
+            ),
+            ListTile(
+              onTap: () {},
+              title: const Text("Newsletter"),
+              leading: const Icon(Icons.newspaper),
+            ),
+            const ListTile(
+              title: Text("Event"),
+            ),
+            const ListTile(
+              title: Text("Members"),
+            ),
+            const ListTile(
+              title: Text("Vetting"),
+            ),
+            const ListTile(
+              title: Text("Payment"),
+            ),
+            const ListTile(
+              title: Text("Product"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
